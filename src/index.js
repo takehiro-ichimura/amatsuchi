e82bc6e514e8b22f96a048417f3617da0a457bbd
takehiro-ichimura <sprollhrmk@gmail.com>
2023-03-03 03:17:13 +0900

    ぶら下げ「。」リサイズ対応

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const burasage = (amatsuchi) => {
     console.log("burasage")
+    const burasageList = ["。", "、"]
     const paras = amatsuchi.getElementsByTagName("p");
     console.log(paras);
     for (let i = 0; i < paras.length; i++) {
         const para = paras[i];
         console.log("***PARA[" + i + "] " + para);
+        para.innerHTML = para.innerHTML.replace('<span class="auto-burasage">。</span>', '。');
         const splitList = (para.innerHTML + "").split("。"); // deep copy
         let resultInnerHTML = "";
         for (let j = 0; j < splitList.length - 1; j++) {
@@ -16,7 +18,7 @@ const burasage = (amatsuchi) => {
             const currentInnerHTML = para.innerHTML + "";
             const currentWidth = para.clientWidth;
             // ぶら下げを代入してみる
-            para.innerHTML = resultInnerHTML + '<span class="burasage">。</span>';
+            para.innerHTML = resultInnerHTML + '<span class="auto-burasage">。</span>';
             // 新しいwidthを測定
             const newWidth = para.clientWidth;
             console.log(currentWidth)
@@ -27,14 +29,32 @@ const burasage = (amatsuchi) => {
                 resultInnerHTML += "。";
             } else {
                 console.log("採用")
-                resultInnerHTML += '<span class="burasage">。</span>';
+                resultInnerHTML += '<span class="auto-burasage">。</span>';
             }
         }
     }
 }
 
+const debounce = (fn, interval) => {
+    var timer
+    return function () {
+        clearTimeout(timer)
+        timer = setTimeout(function () {
+            fn()
+        }, interval)
+    }
+}
+
 const amatsuchiProcess = (amatsuchi) => {
+    let lastHeight = window.innerHeight;
     burasage(amatsuchi);
+    window.addEventListener('resize', function () {
+        if (lastHeight != window.innerHeight) {
+            debounce(burasage(amatsuchi), 1000);
+            lastHeight = window.innerHeight;
+            console.log("height changed: " + lastHeight)
+        }
+    })
 }
 
 const main = () => {
